9f59a037a1f07173e79f7f6ce73c7dced8a257e2
Kris Kumar <kriskumar70@gmail.com>
2019-01-31 22:34:27 -0600

    Add project files.

---
+++ RentAnUmpire.DbContext/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RentAnUmpire.DbContext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RentAnUmpire.Web/Client/README.md
@@ -0,0 +1,4 @@
+# RentaAnUmpire
+
+Angular 7 - Built with with Webpack 4
+
